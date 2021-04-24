a993828fbcc5a763741be4361030600231c26315
Dominic Padula <domopadula2192@gmail.com>
2021-04-24 12:46:19 -0600

    figaro move

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -3,7 +3,7 @@ class MapService
   def self.get_coordinates(location)
     response = Faraday.get('https://www.mapquestapi.com/geocoding/v1/address') do |req|
       req.headers["CONTENT_TYPE"] = "application/json"
-      req.params['key'] = 'JEhEx6kKF90oVbT6xIjpeMD3WoNVXCrz'
+      req.params['key'] = ENV['MAP_API_KEY']
       req.params['location'] = location
     end
     JSON.parse(response.body, symbolize_names: true)
