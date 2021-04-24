89bbab83d919cd6f1571ab633e35239f8c9228d3
Dominic Padula <domopadula2192@gmail.com>
2021-04-24 12:46:12 -0600

    move api to enviornment

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -3,7 +3,7 @@ class ForecastService
   def self.get_forecast(lat, lng)
     response = Faraday.get('https://api.openweathermap.org/data/2.5/onecall?') do |req|
       req.headers["CONTENT_TYPE"] = "application/json"
-      req.params['appid'] = '618a4595b512d53e1ec40ec117d516fc'
+      req.params['appid'] = ENV['FORECAST_API_KEY']
       req.params['lat'] = lat
       req.params['lon'] = lng
       req.params['units'] = 'imperial'
