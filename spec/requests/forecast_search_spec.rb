e91fcecd35c08422c4a9f43c1d5a7462db62415f
Dominic Padula <domopadula2192@gmail.com>
2021-04-24 12:28:18 -0600

    add coverage to ignore

--- spec/requests/forecast_search_spec.rb
+++ spec/requests/forecast_search_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe 'Forecast Search'do
        get '/api/v1/forecast?location=denver,co'
        expect(response.status).to eq(200)
        data = JSON.parse(response.body, symbolize_names: true)
+       expect(data).to be_a(Hash)
+       check_hash_structure(data, :data, Hash)
+       expect(data[:data].keys).to match_array(%i[id type attributes])
+       check_hash_structure(data[:data], :id, NilClass)
+       check_hash_structure(data[:data], :type, String)
+       expect(data[:data][:type]).to eq('forecast')
+       check_hash_structure(data[:data], :attributes, Hash)
+       expect(data[:data][:attributes].keys).to match_array(%i[current_weather daily_weather hourly_weather])
+
+       attributes = data[:data][:attributes]
     end
   end
 end
