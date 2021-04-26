75d434f8ab36a195c66d839cd9671f1bd4a54b0f
Dominic Padula <domopadula2192@gmail.com>
2021-04-26 09:10:03 -0600

    firt request test

---
+++ spec/requests/salary_search_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Salaries Request' do
+  describe 'happy path' do
+    it "can return salaries" do
+      get '/api/v1/salaries?destination=chicago'
+      expect(response.status).to eq(200)
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data).to be_a(Hash)
+      expect(data[:data]).to be_a(Hash)
+      expect(data[:data].keys).to match_array(%i[id type attributes])
+      expect(data[:data][:id]).to eq(nil)
+      expect(data[:data][:type]).to eq("salaries")
+      expect(data[:data][:attributes]).to be_a(Hash)
+      expect(data[:data][:attributes].keys).to match_array(%i[destination forecast salaries])
+      expect(data[:data][:attributes][:destination]).to eq("chicago")
+      expect(data[:data][:attributes][:forecast]).to be_a(Hash)
+      expect(data[:data][:attributes][:forecast].keys).to match_array(%i[summary temperature])
+
+    end
+  end
+end
