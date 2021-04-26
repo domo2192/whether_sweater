06dde2d8be2e2a0e5825c7b5ca89189ec67c7fe6
Dominic Padula <domopadula2192@gmail.com>
2021-04-26 10:07:30 -0600

    create second facade test for creating salaries piece of data

--- app/facades/salary_facade.rb
+++ app/facades/salary_facade.rb
@@ -1,23 +1,29 @@
 class SalaryFacade
 
   def self.get_salaries(destination)
-    salaries = SalaryService.get_salaries(destination)
+    salaries = SalaryService.get_cities(destination)
     get_coordinates = MapService.get_coordinates(destination)
     coordinates = get_coordinates[:results][0][:locations][0][:latLng]
-    forecast = ForecastService.current_forecast(coordinates[:lat], coordinates[:lng])
+    forecast = ForecastService.get_forecast(coordinates[:lat], coordinates[:lng])
     objectify_data(forecast, salaries)
   end
 
   def self.objectify_data(forecast, salaries)
-    OpenStruct.new({ destination: objectify_current_forecast(forecast[:current])
-                  #    salaries:   objectify_salries(salaries[:daily]),
-                  #    hourly_weather:  objectify_hourly_forecast(forecast[:hourly])
-                  # })
+    OpenStruct.new({ destination: objectify_current_forecast(forecast[:current]),
+                      salaries:   objectify_salaries(salaries)
+                   })
   end
 
   def self.objectify_current_forecast(current_forecast)
-          {           temperature:  current_forecast[:temp],
+                  {     temperature:  current_forecast[:temp],
                       conditions:   current_forecast[:weather][0][:description]
                   }
+  end
+
+  def self.objectify_salaries(salaries)
+    require "pry"; binding.pry
+  #   {     title:  salaries[:],
+  #       conditions:   current_forecast[:weather][0][:description]
+  #   }
   end
 end
--- spec/facades/salary_facade_spec.rb
+++ spec/facades/salary_facade_spec.rb
@@ -17,8 +17,32 @@ RSpec.describe 'Salary Facade' do
                        :wind_deg=>36,
                        :wind_gust=>7,
                        :weather=>[{:id=>804, :main=>"Clouds", :description=>"overcast clouds", :icon=>"04d"}]}
-    expect(WeatherFacade.objectify_current_forecast(current_params).class).to eq(Hash)
-    expect(WeatherFacade.objectify_current_forecast(current_params)[:temperature]).to eq(62.85)
-    expect(WeatherFacade.objectify_current_forecast(current_params)[:conditions]).to eq("overcast clouds")
+    expect(SalaryFacade.objectify_current_forecast(current_params).class).to eq(Hash)
+    expect(SalaryFacade.objectify_current_forecast(current_params)[:temperature]).to eq(62.85)
+    expect(SalaryFacade.objectify_current_forecast(current_params)[:conditions]).to eq("overcast clouds")
+  end
+
+  it 'returns the specific data we want' do
+    test_data = {:_links=>
+  {:curies=>
+    [{:href=>"https://developers.teleport.org/api/resources/Location/#!/relations/{rel}/", :name=>"location", :templated=>true},
+     {:href=>"https://developers.teleport.org/api/resources/City/#!/relations/{rel}/", :name=>"city", :templated=>true},
+     {:href=>"https://developers.teleport.org/api/resources/UrbanArea/#!/relations/{rel}/", :name=>"ua", :templated=>true},
+     {:href=>"https://developers.teleport.org/api/resources/Country/#!/relations/{rel}/", :name=>"country", :templated=>true},
+     {:href=>"https://developers.teleport.org/api/resources/Admin1Division/#!/relations/{rel}/", :name=>"a1", :templated=>true},
+     {:href=>"https://developers.teleport.org/api/resources/Timezone/#!/relations/{rel}/", :name=>"tz", :templated=>true}],
+   :self=>{:href=>"https://api.teleport.org/api/urban_areas/slug:chicago/salaries/"}},
+ :salaries=>
+  [{:job=>{:id=>"CIVIL-ENGINEER", :title=>"Civil Engineer"}, :salary_percentiles=>{:percentile_25=>75046.79203102979, :percentile_50=>89467.74100530887, :percentile_75=>106659.81135187489}},
+   {:job=>{:id=>"CONTENT-MARKETING", :title=>"Content Marketing"}, :salary_percentiles=>{:percentile_25=>39402.36738857876, :percentile_50=>49333.79507025396, :percentile_75=>61768.4544695476}},
+   {:job=>{:id=>"COPYWRITER", :title=>"Copywriter"}, :salary_percentiles=>{:percentile_25=>37874.33329144154, :percentile_50=>46571.95367417419, :percentile_75=>57266.9319969137}},
+   {:job=>{:id=>"CUSTOMER-SUPPORT", :title=>"Customer Support"}, :salary_percentiles=>{:percentile_25=>28709.782495494794, :percentile_50=>36911.39045392457, :percentile_75=>47455.97586661874}},
+   {:job=>{:id=>"DATA-ANALYST", :title=>"Data Analyst"}, :salary_percentiles=>{:percentile_25=>46898.18614517015, :percentile_50=>56442.498784333024, :percentile_75=>67929.18726447425}},
+   {:job=>{:id=>"DATA-SCIENTIST", :title=>"Data Scientist"}, :salary_percentiles=>{:percentile_25=>71025.60310363481, :percentile_50=>85799.94207526707, :percentile_75=>103647.55438088557}}]}
+   expect(SalaryFacade.objectify_salaries(test_data).class).to eq(Array)
+   expect(SalaryFacade.objectify_salaries(test_data)[0].keys).to match_array(%i[title min max])
+   expect(SalaryFacade.objectify_salaries(test_data)[0][:title]).to eq("Data Analyst")
+   expect(SalaryFacade.objectify_salaries(test_data)[0][:min]).to eq("$46,898.19")
+   expect(SalaryFacade.objectify_salaries(test_data)[0][:max]).to eq("$67,929.19",)
   end
 end
